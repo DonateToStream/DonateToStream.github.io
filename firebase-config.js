3806b9a53613b14510e2f44ceb11bbae6f0a9d6f
DonateToStream <behehswhhwhw@gmail.com>
2025-03-14 03:21:33 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxm9scK9bWxue782r8w2xNR_thS1y9-q4",
+  authDomain: "dhjrjtdzjg.firebaseapp.com",
+  databaseURL: "https://dhjrjtdzjg-default-rtdb.firebaseio.com",
+  projectId: "dhjrjtdzjg",
+  storageBucket: "dhjrjtdzjg.firebasestorage.app",
+  messagingSenderId: "548960975194",
+  appId: "1:548960975194:web:fc9596a8cae353883d9b63",
+  measurementId: "G-BPJJSQV4Z6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
