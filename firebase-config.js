6e884055a1171d726cf0da74188ae4967e1d5924
DonateToStream <behehswhhwhw@gmail.com>
2025-03-14 23:09:12 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
+// Load Firebase Configuration in "compat" mode
 const firebaseConfig = {
-  apiKey: "AIzaSyBxm9scK9bWxue782r8w2xNR_thS1y9-q4",
-  authDomain: "dhjrjtdzjg.firebaseapp.com",
-  databaseURL: "https://dhjrjtdzjg-default-rtdb.firebaseio.com",
-  projectId: "dhjrjtdzjg",
-  storageBucket: "dhjrjtdzjg.firebasestorage.app",
-  messagingSenderId: "548960975194",
-  appId: "1:548960975194:web:fc9596a8cae353883d9b63",
-  measurementId: "G-BPJJSQV4Z6"
+    apiKey: "AIzaSyBxm9scK9bWxue782r8w2xNR_thS1y9-q4",
+    authDomain: "dhjrjtdzjg.firebaseapp.com",
+    databaseURL: "https://dhjrjtdzjg-default-rtdb.firebaseio.com",
+    projectId: "dhjrjtdzjg",
+    storageBucket: "dhjrjtdzjg.appspot.com",
+    messagingSenderId: "548960975194",
+    appId: "1:548960975194:web:fc9596a8cae353883d9b63",
+    measurementId: "G-BPJJSQV4Z6"
 };
 
-// Initialize Firebase (global firebase object)
+// Initialize Firebase (using compat mode)
 firebase.initializeApp(firebaseConfig);
